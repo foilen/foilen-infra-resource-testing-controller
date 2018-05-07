b1e68d6c2b25151945f02e320beaf44ab6a4c70e
Simon Levesque <simon@foilen.com>
2018-05-07 16:11:27 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Foilen Infra Resource Testing Controller
+==============
+
+A plugin with some resources for testing purpose.
+
+Build
+-----
+
+./create-local-release.sh
---
+++ src/main/java/com/foilen/infra/resource/testing/controller/TestingControllerPluginDefinitionProvider.java
@@ -0,0 +1,59 @@
+/*
+    Foilen Infra Resource Example
+    https://github.com/foilen/foilen-infra-resource-testing-controller
+    Copyright (c) 2018 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.infra.resource.testing.controller;
+
+import com.foilen.infra.plugin.v1.core.context.CommonServicesContext;
+import com.foilen.infra.plugin.v1.core.plugin.IPPluginDefinitionProvider;
+import com.foilen.infra.plugin.v1.core.plugin.IPPluginDefinitionV1;
+
+/**
+ * A plugin to test.
+ */
+public class TestingControllerPluginDefinitionProvider implements IPPluginDefinitionProvider {
+
+    static private TestingControllerPluginDefinitionProvider instance;
+
+    public static TestingControllerPluginDefinitionProvider getInstance() {
+        return instance;
+    }
+
+    private TestingControllerInfiniteLoopUpdateHander testingControllerInfiniteLoopUpdateHander = new TestingControllerInfiniteLoopUpdateHander();
+    private TestingControllerMockUpdateHander testingControllerMockUpdateHander = new TestingControllerMockUpdateHander();
+
+    public TestingControllerPluginDefinitionProvider() {
+        instance = this;
+    }
+
+    @Override
+    public IPPluginDefinitionV1 getIPPluginDefinition() {
+
+        IPPluginDefinitionV1 pluginDefinitionV1 = new IPPluginDefinitionV1("Foilen", "Testing Controller", "To do some specific tests", "1.0.0");
+        pluginDefinitionV1.addUpdateHandler(testingControllerInfiniteLoopUpdateHander);
+        pluginDefinitionV1.addUpdateHandler(testingControllerMockUpdateHander);
+        return pluginDefinitionV1;
+    }
+
+    public TestingControllerInfiniteLoopUpdateHander getTestingControllerInfiniteLoopUpdateHander() {
+        return testingControllerInfiniteLoopUpdateHander;
+    }
+
+    public TestingControllerMockUpdateHander getTestingControllerMockUpdateHander() {
+        return testingControllerMockUpdateHander;
+    }
+
+    @Override
+    public void initialize(CommonServicesContext commonServicesContext) {
+    }
+
+    public void setTestingControllerUpdateHander(TestingControllerInfiniteLoopUpdateHander testingControllerUpdateHander) {
+        this.testingControllerInfiniteLoopUpdateHander = testingControllerUpdateHander;
+    }
+
+}
