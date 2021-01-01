6c2e5d83cd0064f8217206933615ea5e844c293f
Simon Levesque <simon@foilen.com>
2021-01-01 18:04:13 -0500

    Update copyrights

--- src/main/java/com/foilen/infra/resource/testing/controller/TestingControllerMockChangesEventHandler.java
+++ src/main/java/com/foilen/infra/resource/testing/controller/TestingControllerMockChangesEventHandler.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra Resource Example
     https://github.com/foilen/foilen-infra-resource-testing-controller
-    Copyright (c) 2018-2020 Foilen (http://foilen.com)
+    Copyright (c) 2018-2021 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
