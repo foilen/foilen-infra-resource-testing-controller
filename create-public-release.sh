ab3e25c3e784e7198b6dcd1451aed9ef1fa69093
Simon Levesque <simon@foilen.com>
2021-02-06 20:09:07 -0500

    Switching to Maven Central

--- create-public-release.sh
+++ create-public-release.sh
@@ -22,10 +22,17 @@ export VERSION=$1
 
 ./step-update-copyrights.sh
 ./step-clean-compile.sh
-./step-upload-bintray.sh
+./step-upload-ossrh.sh
 ./step-git-tag.sh
 
 echo ----[ Operation completed successfully ]----
 
 echo
+echo To deploy the new release:
+echo '- go on https://oss.sonatype.org/#stagingRepositories'
+echo '- close it (wait for it to be closed)'
+echo '- release it'
+echo 
+echo Then, can see published items on 
+echo https://repo1.maven.org/maven2/com/foilen/foilen-infra-resource-testing-controller/
 echo You can send the tag: git push --tags
